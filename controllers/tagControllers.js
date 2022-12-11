96bc79da72d3eb6d0a3fd6b99379846bdd2eb92b
Cuthbert255 <71035900+Cuthbert255@users.noreply.github.com>
2022-12-12 01:34:26 +0300

    Finished getTags Controller

--- controllers/tagControllers.js
+++ controllers/tagControllers.js
@@ -5,25 +5,37 @@ exports.createTag = async (req, res, next) => {
   // Check for errors
   const errors = validationResult(req);
 
-  !errors.isEmpty() && res.status(400).json({ errors: errors.array() });
-
-  // Create new tag
-  try {
-    const { name } = req.body;
-    const data = { name };
-    const result = await prisma.tag.create({ data });
-    console.log(result);
-    result &&
-      res
-        .status(200)
-        .json({ message: "SUCCESS", resultCode: 0, result: result });
-  } catch (error) {
-    next(error);
+  if (!errors.isEmpty()) {
+    res.status(400).json({ errors: errors.array() });
+  } else {
+    // Create new tag
+    try {
+      const { name } = req.body;
+      const data = { name };
+      const result = await prisma.tag.create({ data });
+      console.log(result);
+      result &&
+        res
+          .status(200)
+          .json({ message: "SUCCESS", resultCode: 0, result: result });
+    } catch (error) {
+      next(error);
+    }
   }
 };
 
 exports.getTags = async (req, res, next) => {
   // Get all tags
-  console.log("getting all tags");
+  try {
+    const result = await prisma.tag.findMany();
+
+    if (!result) {
+      res.status(404).json({ errorCode: 1020, message: "Tags not found" });
+    } else {
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
 
